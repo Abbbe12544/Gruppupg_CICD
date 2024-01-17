2441c5b8afeb210940ecdbec19b641d116a51ca6
Isak <Isak>
2024-01-17 10:38:36 +0100

    FINALY IT WORK HAPY

--- Program Tester.cs
+++ Program Tester.cs
@@ -1,87 +1,33 @@
-﻿using System; 
-using static System.Console; 
-using Xunit; 
+﻿using Xunit; 
 
-namespace application
+namespace PersonnummerKontroll.Tests
 {
-    
-
-    public class PersonnummerTest 
+    public class PersonnummerVerifieraTests
     {
-        
         [Fact]
-        public void TestLength()
+        public void Verifiera_GiltigtPersonnummer_ReturnerarTrue()
         {
-            //Arrange
-            var validator = new Program.IsValidSwedishPersonalNumber("050405-1419");
-            string validPersonnummer = "910101-1234";
-            string invalidPersonnummer = "12345678901"; 
+            //Given
+            string personnummer = "8808214855";
 
-            // Act
-            var validResult = validator.Validate(validPersonnummer);
-            var invalidResult = validator.Validate(invalidPersonnummer);
+            // When
+            bool result = PersonnummerVerifiera.Verifiera(personnummer);
 
-            // Assert
-            Assert.True(validResult); // Kontrollera att giltiga personnummer accepteras
-
-            // Kontrollera att ogiltiga personnummer (för långa) inte accepteras
-            Assert.False(invalidResult, "Personnummer längre än 10 tecken borde vara ogiltiga.");
-        }
-        
-        /*[Fact]
-        public void TestControlNum()
-        {
-            // Arrange
-            var validator = new Program.IsValidSwedishPersonalNumber();
-            string validPersonnummer = "910101-1234";
-            string invalidPersonnummer = "910101-5678";
-
-            // Act
-            var validResult = validator.ValidateControlNum(validPersonnummer);
-            var invalidResult = validator.ValidateControlNum(invalidPersonnummer);
-
-            // Assert
-            Assert.True(validResult); // Kontrollera att giltiga kontrollsiffror accepteras
-
-            // Kontrollera att ogiltiga kontrollsiffror inte accepteras
-            Assert.False(invalidResult, "Ogiltig kontrollsiffra borde inte accepteras.");
+            // Then
+            Assert.True(result);
         }
 
         [Fact]
-        public void TestBirthPlace()
+        public void Verifiera_OgiltigtPersonnummer_ReturnerarFalse()
         {
-            // Arrange
-            var validator = new Program.IsValidSwedishPersonalNumber();
-            string validPersonnummer = "910101-1234";
-            string invalidPersonnummer = "920202-5678";
+            // Given
+            string personnummer = "9003994855";
 
-            // Act
-            var validResult = validator.ValidateBirthPlace(validPersonnummer);
-            var invalidResult = validator.ValidateBirthPlace(invalidPersonnummer);
+            // When
+            bool result = PersonnummerVerifiera.Verifiera(personnummer);
 
-            // Assert
-            Assert.True(validResult); // Kontrollera att giltigt födelseort accepteras
-
-            // Kontrollera att ogiltigt födelseort inte accepteras
-            Assert.False(invalidResult, "Ogiltig födelseort borde inte accepteras.");
+            // Then
+            Assert.False(result);
         }
-
-        [Fact]
-        public void TestGender()
-        {
-            // Arrange
-            var validator = new Program.IsValidSwedishPersonalNumber();
-            string malePersonnummer = "910101-1234";
-            string femalePersonnummer = "910101-5678";
-
-            // Act
-            var maleResult = validator.ValidateGender(malePersonnummer, Gender.Male);
-            var femaleResult = validator.ValidateGender(femalePersonnummer, Gender.Female);
-
-            // Assert
-            Assert.True(maleResult); // Kontrollera att manliga personnummer accepteras
-            Assert.True(femaleResult); // Kontrollera att kvinnliga personnummer accepteras
-        }*/
     }
-
 }
