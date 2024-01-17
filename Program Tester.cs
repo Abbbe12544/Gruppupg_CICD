778c86dcf91607968522465c1a1c4beee1b933ea
Isak <Isak>
2024-01-17 10:18:58 +0100

    Test build 12

--- Program Tester.cs
+++ Program Tester.cs
@@ -13,7 +13,7 @@ namespace application
         public void TestLength()
         {
             //Arrange
-            var validator = new Program.Program;
+            var validator = new Program.IsValidSwedishPersonalNumber("050405-1419");
             string validPersonnummer = "910101-1234";
             string invalidPersonnummer = "12345678901"; 
 
